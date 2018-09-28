ccff9e05ad0191886522c0dfe9ab9c9a42f7b0d5
statrsssystemm <43653165+statrsssystemm@users.noreply.github.com>
2018-09-28 11:55:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -171,6 +171,22 @@ client.on("message", message => {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "welcome");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 client.on('messageDelete', message => {
  
     if(message.author.bot) return;
